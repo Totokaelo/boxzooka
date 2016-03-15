5b45e65155053875639ce536553f99de119f5c60
Quinton Harris <quinton.harris@gmail.com>
2016-03-14 19:07:03 -0700

    Alphabetize fields on Address.

--- lib/boxzooka/address.rb
+++ lib/boxzooka/address.rb
@@ -1,14 +1,5 @@
 module Boxzooka
   class Address < BaseElement
-    # Shipment first name
-    scalar :first_name
-
-    # Shipment last name
-    scalar :last_name
-
-    # Shipment Company
-    scalar :company
-
     # Address Line 1
     scalar :address1
 
@@ -21,24 +12,31 @@ module Boxzooka
     # Shipment City
     scalar :city
 
-    # Shipment State or Province
-    scalar :province
-
-    # Shipment Postal Code
-    scalar :postal_code
+    # Shipment Company
+    scalar :company
 
     # Shipment country code. ISO2.
     scalar :country_code
 
-    # Shipment phone #
-    scalar :phone
-
     # Shipment email address
     scalar :email
 
+    # Shipment first name
+    scalar :first_name
+
+    # Shipment last name
+    scalar :last_name
+
+    # Shipment phone #
+    scalar :phone
+
+    # Shipment Postal Code
+    scalar :postal_code
+
+    # Shipment State or Province
+    scalar :province
+
     # Shipment customer tax ID
     scalar :tax_id, node_name: 'TaxID'
-
-
   end
 end
