b892a55ebb547e9a36da29d58019c7f8cdd0d37e
Quinton Harris <quinton.harris@gmail.com>
2016-05-12 14:29:12 -0700

    Update InboundListResponseItem + InboundRequestItem to not inherit off Item, version bump.

--- lib/boxzooka/inbound_list_response_item.rb
+++ lib/boxzooka/inbound_list_response_item.rb
@@ -1,9 +1,27 @@
 module Boxzooka
   # 'Item' elements on the InboundListResponse.
-  class InboundListResponseItem < Item
-    scalar :quantity
+  class InboundListResponseItem < BaseElement
+    node_name: 'Item'
+
+    # Inbound-specific description of item.
+    scalar :description
 
     # 'data_received', 'arrived', 'received', 'stocked', 'canceled', 'discrepancy', 'unreconciled'
-    scalar :status
+    scalar :item_status
+
+    # Quantity of inbound product.
+    scalar :quantity
+
+    # Received units
+    scalar :received
+
+    # Inbound-specific short description of item.
+    scalar :short_description
+
+    # Item SKU. Primary key.
+    scalar :sku
+
+    # UpcVendorBarcode.
+    scalar :upc_vendor_barcode
   end
 end
--- lib/boxzooka/inbound_request_item.rb
+++ lib/boxzooka/inbound_request_item.rb
@@ -1,7 +1,19 @@
 module Boxzooka
   # Line Items for the InboundRequest.
-  class InboundRequestItem < Item
+  class InboundRequestItem < BaseElement
+    # Inbound-specific description of item.
+    scalar :description
+
     # Quantity of inbound product.
     scalar :quantity
+
+    # Inbound-specific short description of item.
+    scalar :short_description
+
+    # Item SKU. Primary key.
+    scalar :sku
+
+    # UpcVendorBarcode.
+    scalar :upc_vendor_barcode
   end
 end
